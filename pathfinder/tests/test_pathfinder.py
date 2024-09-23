e4ae979dc3bc2705b038102169c8c5db6ab466d0
Saad Jbabdi <saad@winpd0051.home>
2024-09-23 16:38:41 +0100

    added notebooks

--- pathfinder/tests/test_pathfinder.py
+++ pathfinder/tests/test_pathfinder.py
@@ -18,6 +18,11 @@ def test_simulate_data_grid():
 
     data = utils.simulate_data_grid(missing=[(1,0),])
     assert data['Dom1']['Mod0'] is None
+    assert data['Dom0']['Mod0'] is not None
+
+    data, data_c = utils.simulate_data_grid(missing=[(1,0),], output_complete=True)
+    assert (data['Dom1']['Mod0'] is None) and (data_c['Dom1']['Mod0'] is not None)
+    assert (data['Dom0']['Mod0'] is not None) and (data_c['Dom0']['Mod0'] is not None)
 
 def test_JointDecomp():
     data = utils.simulate_data_grid(num_domains=3, num_modalities=2)
@@ -26,3 +31,10 @@ def test_JointDecomp():
     A, S, err = algo.fit(data)
     assert len(A) == 3
     assert len(S) == 2
+
+def test_predicted_data_grid():
+    data = utils.simulate_data_grid(num_domains=3, num_modalities=2)
+    algo = decomp.JointDecomp(n_components=5, n_iter=3)
+    A, S, err = algo.fit(data)
+    grid = utils.predicted_data_grid(data, A, S)
+    assert grid.keys() == data.keys()
--- pathfinder/utils.py
+++ pathfinder/utils.py
@@ -7,11 +7,15 @@
 # Copyright (C) 2024 University of Oxford
 # SHBASECOPYRIGHT
 
+
+# TODO:
+#
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 # SIMULATIONS
-def simulate_data_grid(num_domains=3, num_modalities=2, rank=5, missing=None, noise=0.):
+def simulate_data_grid(num_domains=3, num_modalities=2, rank=5, missing=None, noise=0., output_complete=False):
     """
     :param num_domains: number of domains (rows of data grid)
     :param num_modalities: number of modalities (columns of data grid)
@@ -27,7 +31,6 @@ def simulate_data_grid(num_domains=3, num_modalities=2, rank=5, missing=None, no
         Missing = [(f'Dom{m[0]}',f'Mod{m[1]}') for m in missing]
 
     A, S = {}, {}
-    DataSets = {}
 
     for d in Domains:
         n    = np.random.randint(1000, 2000)
@@ -37,16 +40,43 @@ def simulate_data_grid(num_domains=3, num_modalities=2, rank=5, missing=None, no
         S[m] = np.random.randn(n, rank)/10.
 
     DataSets = {}
+    if output_complete:
+        DataSetsComplete = {}
     for d in Domains:
         DataSets[d] = {}
+        if output_complete:
+            DataSetsComplete[d] = {}
         for m in Modalities:
+            AS = A[d] @ S[m].T + noise*np.random.randn(len(A[d]), len(S[m]))
             if (d, m) in Missing:
                 DataSets[d][m] = None
             else:
-                DataSets[d][m] = A[d] @ S[m].T + noise*np.random.randn(len(A[d]), len(S[m]))
+                DataSets[d][m] = AS
+            if output_complete:
+                    DataSetsComplete[d][m] = AS
+    if output_complete:
+        return DataSets, DataSetsComplete
+    else:
+        return DataSets
 
-    return DataSets
+# Predict data
+def predicted_data_grid(data, A, S):
+    """Predict data grid using X=AS^T
+    :param data: dictionary
+    :param A: list
+    :param S: list
+    :return: dictionary
+    """
+    Domains = list(data.keys())
+    Modalities = list(data[Domains[0]].keys())
 
+    pred = {}
+    for i, d in enumerate(Domains):
+        pred[d] = {}
+        for j, m in enumerate(Modalities):
+            pred[d][m] = A[i] @ S[j].T
+
+    return pred
 
 # VISUALISATION
 def plot_error(err, use_log=True):
@@ -80,4 +110,31 @@ def plot_data_grid(DataGrid):
             axes[i, j].set_xticks([])
             axes[i, j].set_yticks([])
 
+    return fig
+
+def plot_data_fit(data, A, S, data_complete=None):
+    from pathfinder import decomp
+    jd    = decomp.JointDecomp(n_components=5)
+    D, M  = jd.get_dataset_dimensions(data)
+    mask  = jd.get_dataset_mask(data)
+
+    Domains    = list(data.keys())
+    Modalities = list(data[Domains[0]].keys())
+
+    fig, axes  = plt.subplots(D,M)
+    for ax, col in zip(axes[0], Modalities):
+        ax.set_title(col, size='large')
+
+    for ax, row in zip(axes[:, 0], Domains):
+        ax.set_ylabel(row, rotation=90, size='large')
+
+    for i, d in enumerate(data):
+        for j, m in enumerate(data[d]):
+            if data_complete is not None:
+                groudtruth = data_complete[d][m]
+            else:
+                groudtruth = data[d][m]
+            c = '#4c72b0ff' if mask[i, j] else '#dd8452ff'
+            axes[i, j].scatter(groudtruth, A[i]@S[j].T, c=c)
+            axes[i, j].axline((0, 0), slope=1, color='k')
     return fig
