fa5215bc4ec90b23dbbb79ac73cd4aa7669416fc
Saad Jbabdi <saad@winpd0051.local>
2024-10-11 15:12:30 +0100

    refactor code : now interface for JointDecomp is like jointSVD

--- pathfinder/tests/test_pathfinder.py
+++ pathfinder/tests/test_pathfinder.py
@@ -26,34 +26,62 @@ def test_simulate_data_grid():
 
 def test_JointDecomp():
     data = utils.simulate_data_grid(num_domains=3, num_modalities=2)
+    data, alpha, beta = utils.DataTable_to_Lookup(data)
     from sklearn.linear_model import Ridge
-    algo = decomp.JointDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3})
-    A, S, err = algo.fit(data)
-    assert len(A) == 3
-    assert len(S) == 2
+    algo = decomp.JointOuterDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3})
+    algo.fit(data, alpha, beta)
+    assert len(algo._A) == 3
+    assert len(algo._S) == 2
+    # Test with DataDict
+    data = utils.simulate_data_grid(num_domains=3, num_modalities=2)
+    from sklearn.linear_model import Ridge
+    algo = decomp.JointOuterDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3})
+    algo.fit(data)
+    assert len(algo._A) == 3
+    assert len(algo._S) == 2
+
 
 def test_JointDecomp_ICA():
     data = utils.simulate_data_grid(num_domains=3, num_modalities=2)
+    data, alpha, beta = utils.DataTable_to_Lookup(data)
     from sklearn.linear_model import Ridge
     import warnings
     warnings.filterwarnings("ignore")
-    algo = decomp.JointDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3}, do_ica='left')
-    A, S, err = algo.fit(data)
-    assert len(A) == 3
-    assert len(S) == 2
-    algo = decomp.JointDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3}, do_ica='right')
-    A, S, err = algo.fit(data)
-    assert len(A) == 3
-    assert len(S) == 2
-    algo = decomp.JointDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3}, do_ica='both')
-    A, S, err = algo.fit(data)
-    assert len(A) == 3
-    assert len(S) == 2
+    algo = decomp.JointOuterDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3}, do_ica='left')
+    algo.fit(data, alpha, beta)
+    assert len(algo._A) == 3
+    assert len(algo._S) == 2
+    algo = decomp.JointOuterDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3}, do_ica='right')
+    algo.fit(data, alpha, beta)
+    assert len(algo._A) == 3
+    assert len(algo._S) == 2
+    algo = decomp.JointOuterDecomp(n_components=5, n_iter=3, dropout=-1, method=Ridge, method_kwargs={'alpha':1e3}, do_ica='both')
+    algo.fit(data, alpha, beta)
+    assert len(algo._A) == 3
+    assert len(algo._S) == 2
 
 
 def test_predicted_data_grid():
-    data = utils.simulate_data_grid(num_domains=3, num_modalities=2)
-    algo = decomp.JointDecomp(n_components=5, n_iter=3)
-    A, S, err = algo.fit(data)
-    grid = utils.predicted_data_grid(data, A, S)
-    assert grid.keys() == data.keys()
+    data_dict = utils.simulate_data_grid(num_domains=3, num_modalities=2)
+    algo = decomp.JointOuterDecomp(n_components=5, n_iter=3)
+    data_list, alpha, beta = utils.DataTable_to_Lookup(data_dict)
+
+    algo.fit(data_list, alpha, beta)
+
+    grid = utils.predicted_data_grid(data_dict, algo._A, algo._S)
+    assert grid.keys() == data_dict.keys()
+
+
+def test_Lookup():
+    # simulate data
+    data, data_complete = utils.simulate_data_grid(num_domains=3,
+                                               num_modalities=4,
+                                               missing=[(0,1),(0,3),(1,3),(2,1),(2,2)],
+                                               output_complete=True)
+
+
+
+    Clist, alpha, beta = utils.DataTable_to_Lookup(data)
+    data2              = utils.Lookup_to_DataTable(Clist, alpha, beta, list(data.keys()), list(data[list(data.keys())[0]].keys()))
+
+    assert np.all(data2['Dom1']['Mod2'] == data['Dom1']['Mod2'])
--- pathfinder/utils.py
+++ pathfinder/utils.py
@@ -79,6 +79,77 @@ def predicted_data_grid(data, A, S):
 
     return pred
 
+# Lookup stuff Dict-List
+def DataTable_to_Lookup(DataDict):
+    """Produce Clist,alpha/beta so we can use jointSVD
+    """
+    Clist = []
+    alpha = []
+    beta  = []
+    for i, row in enumerate(DataDict):
+        for j, col in enumerate(DataDict[row]):
+            if DataDict[row][col] is not None:
+                Clist.append(DataDict[row][col])
+                alpha.append(i)
+                beta.append(j)
+    return Clist, alpha, beta
+
+def Lookup_to_DataTable(DataList, alpha, beta, row_names=None, col_names=None):
+    """Produce Data Dictionary based on list and lookup (for backwards compatibility)
+    """
+    DataDict = {}
+    n_rows, n_cols = max(alpha)+1, max(beta)+1
+    if row_names is not None:
+        assert n_rows == len(row_names), f'n_rows={n_rows} does not match len(row_names)={len(row_names)}'
+    else:
+        row_names = [f'row_{i}' for i in range(n_rows)]
+    if col_names is not None:
+        assert n_cols == len(col_names), f'n_cols={n_cols} does not match len(col_names)={len(col_names)}'
+    else:
+        col_names = [f'col_{i}' for i in range(n_cols)]
+
+    # init with None
+    for i, row in enumerate(row_names):
+        DataDict[row] = {}
+        for j, col in enumerate(col_names):
+            DataDict[row][col] = None
+
+    for k in range(len(DataList)):
+        i, j = alpha[k], beta[k]
+        DataDict[row_names[i]][col_names[j]] = DataList[k]
+
+    return DataDict
+
+
+
+# ICA
+def perform_ica(A, S, approach='both'):
+    """Do a concat ICA at the end of the fitting process
+
+    Parameters
+    ----------
+    A: list of left-matrices
+    S: list of right-matrices
+    approach (str) : 'left', 'right', or 'both'
+
+    :return: rotated A's and S's
+    """
+    X = []
+    if approach in ['left', 'both']:
+        X.extend(A)
+    if approach in ['right', 'both']:
+        X.extend(S)
+
+    from sklearn.decomposition import FastICA
+    ica = FastICA(whiten=False)
+    X = np.concatenate(X, axis=0)
+    ica.fit(X)
+    # rotate all matrices
+    A = [ica.transform(a) for a in A]
+    S = [ica.transform(s) for s in S]
+    return A, S
+
+
 # VISUALISATION
 def plot_error(err, use_log=True):
     fig = plt.figure()
@@ -113,29 +184,26 @@ def plot_data_grid(DataGrid):
 
     return fig
 
-def plot_data_fit(data, A, S, data_complete=None):
-    from pathfinder import decomp
-    jd    = decomp.JointDecomp(n_components=5)
-    D, M  = jd.get_dataset_dimensions(data)
-    mask  = jd.get_dataset_mask(data)
+def plot_data_fit(data_dict, A, S, data_complete=None):
 
-    Domains    = list(data.keys())
-    Modalities = list(data[Domains[0]].keys())
+    Domains    = list(data_dict.keys())
+    Modalities = list(data_dict[Domains[0]].keys())
 
-    fig, axes  = plt.subplots(D,M)
+    fig, axes  = plt.subplots(len(Domains),len(Modalities))
     for ax, col in zip(axes[0], Modalities):
         ax.set_title(col, size='large')
 
     for ax, row in zip(axes[:, 0], Domains):
         ax.set_ylabel(row, rotation=90, size='large')
 
-    for i, d in enumerate(data):
-        for j, m in enumerate(data[d]):
+    for i, d in enumerate(data_dict):
+        for j, m in enumerate(data_dict[d]):
             if data_complete is not None:
                 groudtruth = data_complete[d][m]
             else:
-                groudtruth = data[d][m]
-            c = '#4c72b0ff' if mask[i, j] else '#dd8452ff'
+                groudtruth = data_dict[d][m]
+
+            c = '#4c72b0ff' if data_dict[d][m] is not None else '#dd8452ff'
             axes[i, j].scatter(groudtruth, A[i]@S[j].T, c=c)
             axes[i, j].axline((0, 0), slope=1, color='k')
     return fig
