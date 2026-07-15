db06663683de9f91c952b20dbfe4a189486202d4
Ying-Qiu Zheng <ying-qiu.zheng@ndcn.ox.ac.uk>
2026-07-15 20:11:02 +0100

    setup ci

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 with open('requirements.txt', 'rt') as f:
-    install_requires = [line.strip() for line in f.readlines()]
+    install_requires = [line.strip() for line in f if line.strip()]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -14,5 +14,6 @@ setup(name='pathfinder',
       author_email='<saad.jbabdi@ndcn.ox.ac.uk>',
       packages=['pathfinder',],
       install_requires=install_requires,
+      python_requires='>=3.10',
       )
 
